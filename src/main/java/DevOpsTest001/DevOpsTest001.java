21efd5ea56649187ba475a269bd3302359c8bdbb
UmaBS-200694 <umabs622@gmail.com>
2025-03-10 16:11:40 +0530

    Stash code testing

--- src/main/java/DevOpsTest001/DevOpsTest001.java
+++ src/main/java/DevOpsTest001/DevOpsTest001.java
@@ -3,9 +3,12 @@ package DevOpsTest001;
 public class DevOpsTest001 {
 
 	public static void main(String[] args) {
-	
+
 		System.out.println("Print it");
-System.out.println("Stash changes test");//Git level changes
+
+		System.out.println("Stash changes test");// Git level changes
+
+		System.out.println("Stash changes testing");// laptop side changes
 
 	}
 
