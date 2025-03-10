e4c4c9dcc71f6e1a70486d7f251459ae7f436888
UmaBS-200694 <umabs622@gmail.com>
2025-03-10 16:05:29 +0530

    First commit

---
+++ src/main/java/DevOpsTest001/DevOpsTest001.java
@@ -0,0 +1,11 @@
+package DevOpsTest001;
+
+public class DevOpsTest001 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Print it");
+
+	}
+
+}
---
+++ src/main/java/come/Test/DevOpsTest001/App.java
@@ -0,0 +1,10 @@
+package come.Test.DevOpsTest001;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
